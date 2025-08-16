178762ccc59127d6d57ede2621eda47b3788472b
Shivang-mishra <mishrashivang819@gmail.com>
2025-08-16 12:53:08 +0530

    datatype

---
+++ 02_datatype.js
@@ -0,0 +1,28 @@
+"use strict" //treat all js code as newer version
+//allert(3+3) //we are use node js not browser
+console.log(3+
+    3
+);//code readability should be high
+
+
+//data types
+let name="shivang"
+let age=22
+let logged=true/false
+
+/* number=>2 to power 53
+bigint
+string=""
+boolean=>true/false
+symbol=>unque
+object
+*/
+console.log(typeof "");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
---
+++ 0_variable.js
@@ -0,0 +1,16 @@
+const accountId=1594;
+let accountEmail="mishrashivang819@.com";
+var accountPassword="1234";
+accountCity="jaipur";
+// accountId=2;
+// console.log(accountId);
+// console.log(accountEmail);
+accountEmail="sarvang@gmail.com"
+accountPassword="5432"
+accountCity="lucknow"
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/*
+prefer not to use var 
+because of essue in block scope and functional scope
+*/
