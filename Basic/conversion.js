a94108e54bb654d0eec0b77f63ee04e401b44aca
shre489 <Shreyasharma20003@gmail.com>
2025-08-21 12:14:49 +0530

    js

---
+++ Basic/conversion.js
@@ -0,0 +1,25 @@
+let score=true;
+// console.log(typeof score);
+// let valuinnumber=Number(score);
+// console.log(typeof(valuinnumber));
+// console.log(valuinnumber);
+
+
+//"33"=>33;
+//"33abs"=>nan
+//true=>0;
+// let isLogedin= ""
+// let BooleanisLogedin=Boolean(isLogedin);
+// console.log(BooleanisLogedin);
+
+
+// 1=>true
+// 0=>false
+// ""=>false
+//"shivang"=>true
+
+let Number= 23;
+let isstring= String(Number)
+console.log(isstring);
+console.log(typeof isstring);
+
