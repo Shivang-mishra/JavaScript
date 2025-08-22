15dd7cd57b2711112fbca0c3e38cafd949ef8ed5
Shivang-mishra <mishrashivang819@gmail.com>
2025-08-22 13:45:55 +0530

    js

---
+++ Basic/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score=true;
+// console.log(typeof score);
+// let valuinnumber=Number(score);
+// console.log(typeof(valuinnumber));
+// console.log(valuinnumber);
+
+
+//"33"=>33;
+//"33abs"=>nan
+//true=>0;
+// lo
+
+// 1=>true
+// 0=>false
+// ""=>false
+//"shivang"=>true
+
+// let Number= 23;
+// let isstring= String(Number)
+// console.log(isstring);
+// console.log(typeof isstring);
+
+//********************************OPERATION */
+
+
+let value=3;
+let isneg = -value
+console.log(isneg);
+ let str1= "shiavng"
+ let str2= " mishra"
+ let str3= str1 + str2
+ console.log(str3);
+ 
+
+ console.log("2"+1);
+ console.log("2"+1+3);
+ console.log(3+"2");
+ console.log(4+"2"+1);
+ console.log(4+2+"1");
+ console.log((3+3)*4%2);
+ 
+ console.log(+true);
+ 
+console.log(+"");
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter);
