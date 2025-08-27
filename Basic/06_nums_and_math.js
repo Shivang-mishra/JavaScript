6378047784cee59a52d663e33a09a32d862fc7e8
Shivang-mishra <mishrashivang819@gmail.com>
2025-08-27 23:42:36 +0530

    js

---
+++ Basic/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 100;
+// console.log(score);
+
+
+const balance = new Number(400);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+
+const otherNumber= 1123.334
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundrads= 1000000
+// console.log(hundrads.toLocaleString());
+
+
+// **********************MATHS****************************
+
+
+console.log(Math);
+// console.log(Math.abs(-7));
+
+// console.log(Math.round(7.4));
+// console.log(Math.ceil(7.6));
+// console.log(Math.floor(7.6));
+// console.log(Math.min(7,4,3,2,8));
+// console.log(Math.max(7,4,3,2,8));
+console.log(Math.random());
+// console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min= 10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
